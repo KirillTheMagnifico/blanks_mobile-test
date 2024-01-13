198edf7b02580df077568a06c09aadb918d59feb
KirillTheMagnifico <serkirsuperstar@mail.ru>
2024-01-13 14:24:29 +0300

    Modified Project

--- src/test/java/drivers/BrowserstackMobileDriver.java
+++ src/test/java/drivers/BrowserstackMobileDriver.java
@@ -19,27 +19,27 @@ public class BrowserstackMobileDriver implements WebDriverProvider {
         mutableCapabilities.merge(capabilities);
 
         // Set your access credentials
-        mutableCapabilities.setCapability("browserstack.user", "qaguruqaguru_cWEn5u");
-        mutableCapabilities.setCapability("browserstack.key", "JrvHcugRGP42jAPdVsop");
+        mutableCapabilities.setCapability("browserstack.user", "");
+        mutableCapabilities.setCapability("browserstack.key", "");
 
         // Set URL of the application under test
-        mutableCapabilities.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
+        mutableCapabilities.setCapability("app", "");
 
         // Specify device and os_version for testing
-        mutableCapabilities.setCapability("device", "Google Pixel 3");
-        mutableCapabilities.setCapability("os_version", "9.0");
+        mutableCapabilities.setCapability("device", "Google Pixel 7");
+        mutableCapabilities.setCapability("os_version", "14.0");
 
         // Set other BrowserStack mutableCapabilities
-        mutableCapabilities.setCapability("project", "First Java Project");
-        mutableCapabilities.setCapability("build", "browserstack-build-1");
-        mutableCapabilities.setCapability("name", "first_test");
+        mutableCapabilities.setCapability("project", "");
+        mutableCapabilities.setCapability("build", "");
+        mutableCapabilities.setCapability("name", "");
 
 
         // Initialise the remote Webdriver using BrowserStack remote URL
         // and desired mutableCapabilities defined above
         try {
             return new RemoteWebDriver(
-                    new URL("http://hub.browserstack.com/wd/hub"), mutableCapabilities);
+                    new URL(""), mutableCapabilities);
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
--- src/test/java/helpers/Browserstack.java
+++ src/test/java/helpers/Browserstack.java
@@ -8,12 +8,12 @@ public class Browserstack {
     // https://qaguruqaguru_cWEn5u:JrvHcugRGP42jAPdVsop@api.browserstack.com/app-automate/sessions/0bbf61520103a3f9ab6ec22b79d8d0208c2c6ff8.json
 
     public static String getVideoUrl(String sessionId) {
-        String url = format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
+        String url = format("", sessionId);
 
         return given()
                 .log().all()
                 .filter(withCustomTemplates())
-                .auth().basic("qaguruqaguru_cWEn5u", "JrvHcugRGP42jAPdVsop")
+                .auth().basic("", "")
                 .when()
                 .get(url)
                 .then()
